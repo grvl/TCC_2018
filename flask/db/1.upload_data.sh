658617147e5d0a03cffa41a76ec37a01a1383803
Gabriela Lima <gabrielarvlima@gmail.com>
2018-12-05 16:16:14 -0200

    updated upload script to live DB

--- flask/db/1.upload_data.sh
+++ flask/db/1.upload_data.sh
@@ -1,11 +1,11 @@
-ogr2ogr -f "PostgreSQL" PG:"dbname=pesquisa user=lima" "../free_wheels/scripts/python/data/bus_lines_geo.json" -nln bus_line
+ogr2ogr -f "PostgreSQL" PG:"dbname=freewheels user=freewheels host=eclipse.ime.usp.br" "../free_wheels/scripts/python/data/bus_lines_geo.json" -nln bus_line
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=pesquisa user=lima" "../free_wheels/scripts/python/data/estabelecimentos/sp_venues.json" -nln venues
+ogr2ogr -f "PostgreSQL" PG:"dbname=freewheels user=freewheels host=eclipse.ime.usp.br" "../free_wheels/scripts/python/data/estabelecimentos/sp_venues.json" -nln venues
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=pesquisa user=lima" "../free_wheels/scripts/python/data/estabelecimentos/prefeitura/selo-prefeitura-latlong.json" -nln prefeitura_venues
+ogr2ogr -f "PostgreSQL" PG:"dbname=freewheels user=freewheels host=eclipse.ime.usp.br" "../free_wheels/scripts/python/data/estabelecimentos/prefeitura/selo-prefeitura-latlong.json" -nln prefeitura_venues
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=pesquisa user=lima" "../free_wheels/scripts/python/data/vagas/ZonaAzuVagas_DF_ID_latlong.json" -nln zona_azul
+ogr2ogr -f "PostgreSQL" PG:"dbname=freewheels user=freewheels host=eclipse.ime.usp.br" "../free_wheels/scripts/python/data/vagas/ZonaAzuVagas_DF_ID_latlong.json" -nln zona_azul
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=pesquisa user=lima" "../free_wheels/scripts/r/cptm-sp/cptm.shp" -nln cptm -skip-failures
+ogr2ogr -f "PostgreSQL" PG:"dbname=freewheels user=freewheels host=eclipse.ime.usp.br" "../free_wheels/scripts/r/cptm-sp/cptm.shp" -nln cptm -skip-failures
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=pesquisa user=lima" "../free_wheels/scripts/r/metro-sp/metro.shp" -nln metro2 -skip-failures
+ogr2ogr -f "PostgreSQL" PG:"dbname=freewheels user=freewheels host=eclipse.ime.usp.br" "../free_wheels/scripts/r/metro-sp/metro.shp" -nln metro2 -skip-failures
